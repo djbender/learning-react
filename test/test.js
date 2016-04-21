a523632d944dcd0b5892b83c00bf75ceb233530f
Derek Bender <nofeardjb@gmail.com>
2016-04-21 01:42:34 -0500

    basic trueLib example

--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
+var TrueLib = require('../');
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+describe('trueLib', function() {
+  it('is true', function() {
+    expect(TrueLib()).to.equal(true);
   });
 });
